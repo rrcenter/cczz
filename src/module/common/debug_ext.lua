7ad927573085867c3aa9bae2c9bcf186ba66a09a
uhziel <uhziel@gmail.com>
2018-10-31 21:13:19 +0800

    增强dumpNodeTree()

--- src/module/common/debug_ext.lua
+++ src/module/common/debug_ext.lua
@@ -16,11 +16,11 @@ dump(dumpNodeTree)
 
 @param mixed node 根节点
 
-@parma [integer nesting] 输出时的嵌套层级，默认为 3
+@parma [integer nesting] 输出时的嵌套层级，默认为 99
 
 ]]
 dumpNodeTree = function(node, nesting)
-    if type(nesting) ~= "number" then nesting = 3 end
+    if type(nesting) ~= "number" then nesting = 99 end
 
     local traceback = string.split(debug.traceback("", 2), "\n")
     printInfo("dumpNodeTree from: " .. string.trim(traceback[3]))
@@ -39,10 +39,12 @@ dumpNodeTree = function(node, nesting)
         local children = node:getChildren()
         local x, y = node:getPosition()
         local anchor = node:getAnchorPoint()
-        printInfo("%s[%d]cname:%s name:%s pos(%g,%g) anchor(%g,%g) zorder:%d",
+        local size = node:getContentSize()
+        printInfo("%s[%d]cname:%s name:%s pos(%g,%g) anchor(%g,%g) size(%g,%g) zorder:%d",
             _getHead(nest), nest, node.__cname, node:getName(),
             x, y,
             anchor.x, anchor.y,
+            size.width, size.height,
             node:getLocalZOrder())
         if nest >= nesting then
             return
